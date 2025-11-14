46024f50853146278036a0ad8c2b1694db4b8de8
12999367 <12999367@INFO1-01>
2025-11-14 08:42:04 +0100

    continuazione progetto

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ namespace EsercizioVersoAnimali
         int clickGatto = 0;
         int clickMucca = 0;
         int clickCapra = 0;
-        public event EventHandler<EventArgs> AnimaleStanco;
+        public event EventHandler<AnimaleStancoEventArgs> AnimaleStanco;
 
         public Form1()
         {
@@ -19,55 +19,55 @@ namespace EsercizioVersoAnimali
             AnimaleStanco += ImmagineCapraStanca;
         }
 
-        protected virtual void OnAnimaleStanco()
+        protected virtual void OnAnimaleStanco(string animale)
         {
-            AnimaleStanco?.Invoke(this, EventArgs.Empty);
+            AnimaleStanco?.Invoke(this, new AnimaleStancoEventArgs { Animale = animale });
         }
 
         private void Cane_Click(object sender, EventArgs e)
         {
-            SoundPlayer player = new SoundPlayer(@"C:\Users\Utente\Downloads\Canecheabbaia.wav");
-            player.Play();
+            //SoundPlayer player = new SoundPlayer(@"C:\Users\Utente\Downloads\Canecheabbaia.wav");
+            //player.Play();
             clickCane++;
             if (clickCane > 3)
             {
-                OnAnimaleStanco();
+                OnAnimaleStanco("cane");
                 clickCane = 0;
             }
         }
 
         private void Gatto_Click(object sender, EventArgs e)
         {
-            SoundPlayer player = new SoundPlayer(@"C:\Users\Utente\Downloads\Gattochemiagola.wav");
-            player.Play();
+            //SoundPlayer player = new SoundPlayer(@"C:\Users\Utente\Downloads\Gattochemiagola.wav");
+            //player.Play();
             clickGatto++;
             if (clickGatto > 3)
             {
-                OnAnimaleStanco();
+                OnAnimaleStanco("gatto");
                 clickGatto = 0;
             }
         }
 
         private void Mucca_Click(object sender, EventArgs e)
         {
-            SoundPlayer player = new SoundPlayer(@"C:\Users\Utente\Downloads\Muccachemuggisce.wav");
-            player.Play();
+            //SoundPlayer player = new SoundPlayer(@"C:\Users\Utente\Downloads\Muccachemuggisce.wav");
+            //player.Play();
             clickMucca++;
             if (clickMucca > 3)
             {
-                OnAnimaleStanco();
+                OnAnimaleStanco("mucca");
                 clickMucca = 0;
             }
         }
 
         private void Capra_Click(object sender, EventArgs e)
         {
-            SoundPlayer player = new SoundPlayer(@"C:\Users\Utente\Downloads\Capracheurla.wav");
-            player.Play();
+            //SoundPlayer player = new SoundPlayer(@"C:\Users\Utente\Downloads\Capracheurla.wav");
+            //player.Play();
             clickCapra++;
             if (clickCapra > 3)
             {
-                OnAnimaleStanco();
+                OnAnimaleStanco("capra");
                 clickCapra = 0;
             }
         }
@@ -79,22 +79,22 @@ namespace EsercizioVersoAnimali
 
         private void ImmagineCaneStanco(object sender, EventArgs e)
         {
-
+            Cane.Image = Image.FromFile("C:\\Users\\12999367\\source\\repos\\EsercizioVersoAnimali\\Immagini\\canechedorme.jpg");
         }
 
         private void ImmagineGattoStanco(object sender, EventArgs e)
         {
-
+            Gatto.Image = Image.FromFile("C:\\Users\\12999367\\source\\repos\\EsercizioVersoAnimali\\Immagini\\gattochedorme.jfif");
         }
 
         private void ImmagineMuccaStanca(object sender, EventArgs e)
         {
-
+            Mucca.Image = Image.FromFile("C:\\Users\\12999367\\source\\repos\\EsercizioVersoAnimali\\Immagini\\muccachedorme.jfif");
         }
 
         private void ImmagineCapraStanca(object sender, EventArgs e)
         {
-
+            Capra.Image = Image.FromFile("C:\\Users\\12999367\\source\\repos\\EsercizioVersoAnimali\\Immagini\\caprachedorme.jfif");
         }
     }
 }
